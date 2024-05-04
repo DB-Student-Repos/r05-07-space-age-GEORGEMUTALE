8d274bdac81213376dc98e6f99be4f3e8f8e0df1
GEORGEMUTALE <mutalegeorge367@gmail.com>
2024-05-04 11:49:50 +0200

    years at plannets

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,37 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
-
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+    years: f64,
+}
 
 impl From<u64> for Duration {
-    fn from(s: u64) -> Self {
-        unimplemented!("s, measured in seconds: {s}")
+    fn from(seconds: u64) -> Self {
+        Duration {
+            years: seconds as f64 / 31557600.0,
+        }
     }
 }
 
 pub trait Planet {
-    fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({d:?}) to the number of years on this planet for that duration"
-        );
-    }
+    fn years_during(d: &Duration) -> f64;
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+macro_rules! impl_planet {
+    ($planet:ident, $orbital_period:expr) => {
+        pub struct $planet;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+        impl Planet for $planet {
+            fn years_during(d: &Duration) -> f64 {
+                d.years / $orbital_period
+            }
+        }
+    };
+}
+
+impl_planet!(Mercury, 0.2408467);
+impl_planet!(Venus, 0.61519726);
+impl_planet!(Earth, 1.0);
+impl_planet!(Mars, 1.8808158);
+impl_planet!(Jupiter, 11.862615);
+impl_planet!(Saturn, 29.447498);
+impl_planet!(Uranus, 84.016846);
+impl_planet!(Neptune, 164.79132);
